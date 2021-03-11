212857833aa01b0f8be4bcc5f2d77cb67777757d
MugeraH <bwosihughes24@gmail.com>
2021-03-12 00:30:41 +0300

    add fetch request gunction to get data from contantful api

--- js/index.js
+++ js/index.js
@@ -1,6 +1,88 @@
-$(document).ready(function() {
+
+let table = document.getElementById("table");
+
+//Make request to contentful api to get data
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: "wg1ulkn58gj9",
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: "o0J72qDOOxUWepEu9GokeOaLYsyhJq8GTYWP27PwUS4"
+  });
+  client.getEntries()
+  .then((response) => {
+
+    //Get data and store it inside a variable:data
+  let data = response.items
+
+
+
+  //filter data to get list of all products
+let allProducts = []
+ for(i=0; i<data.length;i++){
+  allProducts.push(response.items[i].fields.productName)
+}
+
+  })
+.catch(console.error);
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //console.log(priceList2);
+// priceList.map((item,index)=>{
+
+//   //console.log(item.retailer);
+//   })
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   //login form toggle sign-in and sing-up forms
+$(document).ready(function() {
 $('.sign-up').click(()=>{
   $('#signIn').slideUp(400);
   $('#signUp').show(400);
@@ -10,8 +92,6 @@ $('.sign-in').click(()=>{
   $('#signIn').show(400);
   $('#signUp').slideUp(400);
 }) 
-
-
 
 });
 
